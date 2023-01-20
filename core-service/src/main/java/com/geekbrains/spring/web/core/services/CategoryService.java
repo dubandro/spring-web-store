71d1ad402fc610aceafc04ca65dc6ebaa8e4bc29
dubandro <dubovik@internet.ru>
2023-01-20 02:22:23 +0200

    Spring Framework Web Store. HomeWork lesson8 (testing)

--- core-service/src/main/java/com/geekbrains/spring/web/core/services/CategoryService.java
+++ core-service/src/main/java/com/geekbrains/spring/web/core/services/CategoryService.java
@@ -1,5 +1,7 @@
 package com.geekbrains.spring.web.core.services;
 
+import com.geekbrains.spring.web.api.dto.CategoryDto;
+import com.geekbrains.spring.web.core.converters.CategoryConverter;
 import com.geekbrains.spring.web.core.entities.Category;
 import com.geekbrains.spring.web.core.repositories.CategoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,5 +16,9 @@ public class CategoryService {
 
     public Optional<Category> findByTitle(String title) {
         return categoryRepository.findByTitle(title);
+    }
+
+    public void saveNewCategory(Category category) {
+        categoryRepository.saveAndFlush(category);
     }
 }
