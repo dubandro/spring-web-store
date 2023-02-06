3ee727c9f09ed1069a22e656aa136f56fbb2c864
dubandro <dubovik@internet.ru>
2023-02-06 18:39:32 +0200

    Spring Framework Web Store. HomeWork lesson12 (Schedulling in mc Statistics)

--- stat-service/src/main/java/com/geekbrains/spring/web/stat/configs/AppConfig.java
+++ stat-service/src/main/java/com/geekbrains/spring/web/stat/configs/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
@@ -16,6 +17,7 @@ import reactor.netty.tcp.TcpClient;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
+@EnableScheduling
 @EnableConfigurationProperties(CoreServiceIntegrationProperties.class)
 @RequiredArgsConstructor
 public class AppConfig {
